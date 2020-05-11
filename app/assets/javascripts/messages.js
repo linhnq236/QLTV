423f423aa3592a70f1fe3f7313173935d9d1f091
LNQ <linhnq36@gmail.com>
2020-05-11 23:00:58 +0700

    chat message using socket

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,26 @@
 $( document ).on('turbolinks:load', function() {
+  var myscroll = $("#messages");
+  myscroll.scrollTop(myscroll.get(0).scrollHeight);
+  var html = '';
+  $.ajax({
+    type: "GET",
+    url: "/api/messages",
+    success: function(reponsive){
+      $.each(reponsive["data"], function(index,value){
+        html += `<p><b>${value["name"]}</b>: ${value["content"]}</p>`
+      })
+      $("#messages").html(html);
+    },
+    error: function(reponsive){
+      console.log(reponsive["data"]);
+    }
+  })
+  $(".chatmes").click(function(){
+    $(".form_chat").show();
+  })
+  $(".close").click(function(){
+    $(".form_chat").hide();
+  })
   $(".submit").click(function(){
     var mes = $.trim($(".mes").val());
     if(mes == ""){
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,10 @@
 module Api
   class MessagesController < ApplicationController
     skip_before_action :verify_authenticity_token
+    def index
+      messages = Message.joins(:user).select("messages.*,users.name")
+      render json: {data: messages}
+    end
     def create
       @message = Message.new(content: params[:message],user_id: current_user.id)
       if @message.save
