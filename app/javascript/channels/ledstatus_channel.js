46c542f25af2bf9470a93cda2fa9744a953295dd
LNQ <linhnq36@gmail.com>
2020-06-03 20:16:34 +0700

    firebase

---
+++ app/javascript/channels/ledstatus_channel.js
@@ -0,0 +1,25 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("LedstatusChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    var setvalue = '';
+    $.each(data["ledstatus"], function(index, value){
+      $(".led").each(function(index1, value1){
+        var res = $(this).data("status");
+        if(index == res){
+          $(this).text(value);
+		console.log(value);
+        }
+      })
+    })
+  }
+});
